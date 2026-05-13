ca0f4d26c45cd2501f0c53d15ad932521b6986f7
Sébastien Crozet <sebcrozet@dimforge.com>
2026-05-13 12:49:14 +0200

    feat: add slice indexing

--- src_rbd_shaders/utils/slice.rs
+++ src_rbd_shaders/utils/slice.rs
@@ -1,3 +1,4 @@
+use core::ops::{Index, IndexMut};
 use khal_std::index::MaybeIndexUnchecked;
 
 // Actual rust slices &array[a..b] don’t compile with rust-gpu, so we
@@ -13,6 +14,11 @@ impl<'a, T: Copy> Slice<'a, T> {
     #[inline]
     pub fn read(&self, i: usize) -> T {
         self.0.read(self.1 + i)
+    }
+
+    #[inline]
+    pub fn offset(self, offset: usize) -> Self {
+        Slice(self.0, self.1 + offset)
     }
 }
 
@@ -39,5 +45,31 @@ impl<'a, T: Copy> SliceMut<'a, T> {
     #[inline]
     pub fn write(&mut self, i: usize, value: T) {
         self.0.write(self.1 + i, value)
+    }
+
+    #[inline]
+    pub fn offset(self, offset: usize) -> Self {
+        SliceMut(self.0, self.1 + offset)
+    }
+}
+
+
+impl<T: Copy> Index<usize> for Slice<'_, T> {
+    type Output = T;
+    fn index(&self, i: usize) -> &T {
+        self.0.at(self.1 + i)
+    }
+}
+
+impl<T: Copy> Index<usize> for SliceMut<'_, T> {
+    type Output = T;
+    fn index(&self, i: usize) -> &T {
+        self.0.at(self.1 + i)
+    }
+}
+
+impl<T: Copy> IndexMut<usize> for SliceMut<'_, T> {
+    fn index_mut(&mut self, i: usize) -> &mut T {
+        self.0.at_mut(self.1 + i)
     }
 }
